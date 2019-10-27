6a5480a8029c59a954a44ebc5e3673c024c04658
aryelciu001 <ryelcius.alfredo29@gmail.com>
2019-10-27 18:45:35 +0800

    BACKEND

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+const KEYS = {
+  publitio: {
+    apiKey: "ec6xX9SjH0W2l3D8mW9r",
+    apiSecret: "AtvvVrvQjpNZQ3Gmo2cWqPciQkHpWSZM"
+  }
+};
+
+module.exports = KEYS;
---
+++ index.js
@@ -0,0 +1,37 @@
+const ex = require("express");
+const app = ex();
+
+const xml = require("xml");
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:3000"
+  })
+);
+
+const keys = require("./config/keys");
+
+const PublitioAPI = require("publitio_js_sdk").default;
+const publitio = new PublitioAPI(keys.publitio.apiKey, keys.publitio.apiSecret);
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.get("/images", (req, res) => {
+  const category = req.params.category;
+  publitio
+    .call("/files/list", "GET", { offset: "0", limit: "1000" })
+    .then(response => {
+      res.send(response.files);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`listening to ${port}`);
+});
