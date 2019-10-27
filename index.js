b6ef0d2ee048397b6ff5ef961735d4d8a0ee4ff2
aryelciu001 <ryelcius.alfredo29@gmail.com>
2019-10-27 18:58:01 +0800

    OK

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ app.get("/images", (req, res) => {
     });
 });
 
+app.get("*", (req, res) => {
+  res.send("hello");
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`listening to ${port}`);
